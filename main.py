83f7986cb3b8d927b77416aa79e5d99fae60cda9
Bastian <badmaxyt@gmail.com>
2024-12-03 19:39:37 +0100

    feat: :sparkles: create experiment in project and attach dataset

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ client = Client(
 )
 dataset = client.get_dataset_version_by_id("0193688e-aa8f-7cbe-9396-bec740a262d0")
 dataset.list_assets().download("./datasets")
+
+project = client.get_project(project_name="Groupe_7")
+
+experiment = project.create_experiment(
+    name="experiment-0",
+    description="base experiment",
+)
+
+experiment.attach_dataset(
+    name="⭐️ cnam_product_2024",
+    dataset_version=dataset,
+)
