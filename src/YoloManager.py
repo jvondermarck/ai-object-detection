bf309ec941f39b87769985b51dcf04fe48aca8fa
Bastian <badmaxyt@gmail.com>
2025-01-07 13:47:00 +0100

    feat: refactor YOLO evaluation process and add model evaluation to Picsellia

--- src/YoloManager.py
+++ src/YoloManager.py
@@ -1,6 +1,9 @@
+import os
+
 import torch
+import yaml
 from picsellia import Experiment
-from picsellia.types.enums import LogType
+from picsellia.types.enums import AddEvaluationType, InferenceType, LogType
 from ultralytics import YOLO
 
 
@@ -42,13 +45,74 @@ class YOLOManager:
         """
         self.model.train(data=config_path, project=project_path, **hyperparameters)
 
-    def evaluate(self, config_path: str) -> None:
-        """Evaluates the YOLO model.
+    def evaluate_metrics(self, config_path: str) -> None:
+        """Evaluates the YOLO model and logs the metrics to Picsellia.
 
         Args:
             config_path (str): Path to the dataset configuration file.
         """
+        print("Evaluating the model...")
+        print("Config path:", config_path)
         self.model.val(data=config_path)
+
+    def evaluate_model(self, config_path: str) -> None:
+        """Evaluates the YOLO model and adds the evaluation to the Picsellia experiment.
+
+        Args:
+            config_path (str): Path to the dataset configuration file.
+        """
+
+        testing_dataset = self.experiment.get_dataset(name="⭐️ cnam_product_2024")
+
+        # Retrieve the labels from the Picsellia dataset
+        picsellia_labels_name = testing_dataset.list_labels()
+        label_matching = {k.name: k for k in picsellia_labels_name}
+        model = YOLO("best.pt")
+
+        # Load the configuration file
+        with open(config_path, "r") as file:
+            config_yaml = yaml.safe_load(file)
+
+        test_images_dir = config_yaml["test"]
+
+        # Iterate over the test images
+        for image_name in os.listdir(test_images_dir):
+            image_path = os.path.join(test_images_dir, image_name)
+
+            # Perform prediction with YOLO
+            results = model.predict(image_path)
+
+            # Retrieve the corresponding asset in Picsellia
+            asset = testing_dataset.find_asset(id=image_name.split(".")[0])
+
+            rectangles = []
+            for result in results:
+                for box, cls, conf in zip(
+                    result.boxes.xywh.tolist(),
+                    result.boxes.cls.tolist(),
+                    result.boxes.conf.tolist(),
+                ):
+                    x_center, y_center, w, h = box
+                    x = x_center - w / 2
+                    y = y_center - h / 2
+                    class_id = int(cls)
+                    label_name = result.names[class_id]
+                    label = label_matching[label_name]
+                    rectangles.append(
+                        (int(x), int(y), int(w), int(h), label, float(conf))
+                    )
+
+            # Add the evaluation to the experiment
+            self.experiment.add_evaluation(
+                asset=asset,
+                add_type=AddEvaluationType.REPLACE,
+                rectangles=rectangles,
+            )
+
+        # Calculate evaluation metrics
+        self.experiment.compute_evaluations_metrics(InferenceType.OBJECT_DETECTION)
+
+        print("Evaluations completed and logged in Picsellia.")
 
     def add_callbacks(self) -> None:
         """Adds custom callbacks to the model."""
